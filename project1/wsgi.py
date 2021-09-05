817cbeca7232ed45da6c72241830a362a4ce149e
Chetan45s <chetansalmotra45@gmail.com>
2021-09-05 20:58:47 +0530

    udpate

--- project1/wsgi.py
+++ project1/wsgi.py
@@ -7,10 +7,25 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project1.settings')
+
+# application = get_wsgi_application()
+
+# +++++++++++ DJANGO +++++++++++
+# To use your own Django app use code like this:
 import os
+import sys
+
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/projectnf/Project1'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'project1.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project1.settings')
-
 application = get_wsgi_application()
