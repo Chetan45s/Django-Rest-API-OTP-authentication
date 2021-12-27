6423f59cc3f401d2950d88cb8ee9f379227fa54b
Chetan45s <chetansalmotra45@gmail.com>
2021-12-28 00:58:10 +0530

    delete friends

--- friends/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from friends.models import Author
-
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
-    pass
--- friends/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Author(models.Model):
-    name = models.CharField(max_length=50)
-    book = models.CharField(max_length=50)
-    is_valid = models.BooleanField()
-
-    def __str__(self):
-        return f"{self.name}"
